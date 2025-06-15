ad128e94df846fbe8860ea0a4187e2a3e2d08a9f
LuAllen1122 <62083692+AllenLu112220@users.noreply.github.com>
2025-06-15 14:02:11 -0600

    good point in front end

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# JobSkillsFrontend
+# Frontend
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.2.
 
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'job-skills-frontend';
+  protected title = 'frontend';
 }
